b2ffbec5f2d2d7e179748388f8e6100ddfabdc30
Michael Klein <Michaelklein1997@gmail.com>
2022-02-25 16:09:45 -0600

    reaction schema created

---
+++ models/Reaction.js
@@ -0,0 +1,27 @@
+const { ObjectId } = require('bson');
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type:ObjectId,
+            default: new ObjectId,
+        },
+        reactionBody: {
+            type:String,
+            required:true,
+            maxlength:280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    }
+)
+
+module.exports = reactionSchema;
