d28dfe757819debabee333b0b8b4bd03beaa2da2
Michael Klein <Michaelklein1997@gmail.com>
2022-02-26 10:45:15 -0600

    created foundation for thought controller functions

---
+++ controllers/thoughtController.js
@@ -0,0 +1,50 @@
+const { Thought } = require('../models');
+
+const getThoughts = (req, res) => {
+    Thought.find()
+        // .populate({
+        //     path: 'thoughts',
+        //     select: '-__V'
+        // })
+        .select('-__v')
+        .then(thoughtData => res.status(200).json(thoughtData))
+        .catch(err => res.status(500).json(err))
+}
+
+const getSingleThought = (req, res) => {
+    Thought.findOne({ _id: req.params.id })
+        // .populate({
+        //     path: 'thoughts',
+        //     select: '-__V'
+        // })
+        .select('-__v')
+        .then(thoughtData => {
+            !thoughtData ? res.status(404).json('Thought does not exist') : res.status(200).json(thoughtData)
+        })
+        .catch(err => res.status(500).json(err))
+} 
+
+const createThought = (req, res) => {
+    Thought.create(req.body)
+    .then(thoughtData => res.status(200).json(thoughtData))
+    .catch(err => res.status(500).json(err))
+}
+
+const updateThought = (req, res) => {
+    Thought.findOneAndUpdate(
+        {_id: req.params.id},
+        req.body
+    )
+    .then(thoughtData => !thoughtData ? res.status(404).json('Thought does not exist') : res.status(200).json(thoughtData))
+    .catch(err => res.status(500).json(err))
+}
+
+const deleteThought = (req, res) => {
+    Thought.deleteOne(
+        {_id: req.params.id}
+    )
+    .then(thoughtData => !thoughtData ? res.status(404).json('Thought does not exist') : res.status(200).json(thoughtData))
+    .catch(err => res.status(500).json(err))
+}
+
+module.exports = { getThoughts, getSingleThought, createThought, updateThought, deleteThought }
