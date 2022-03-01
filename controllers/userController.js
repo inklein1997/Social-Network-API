d0b144d289f6e1df7964582f3c3c914d0f2d5486
Michael Klein <Michaelklein1997@gmail.com>
2022-02-28 21:00:21 -0600

    all promises have been converted to async awaits

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,80 @@
 const { User } = require('../models');
 
-const getUsers = (req, res) => {
-    User.find()
-        .then(userData => res.status(200).json(userData))
-        .catch(err => res.status(500).json(err))
+const getUsers = async (req, res) => {
+    try {
+        const userData = await User.find()
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
 
-const getSingleUser = (req, res) => {
-    User.findOne({ _id: req.params.id })
-        .select('-__v')
-        .populate('thoughts')
-        .populate('friends')
-        .then(userData => {
-            !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData)
-        })
-        .catch(err => res.status(500).json(err))
+const getSingleUser = async (req, res) => {
+    try {
+        const userData = await User.findOne({ _id: req.params.id })
+            .select('-__v')
+            .populate('thoughts')
+            .populate('friends')
+        !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
 
-const createUser = (req, res) => {
-    User.create(req.body)
-        .then(userData => res.status(200).json(userData))
-        .catch(err => res.status(500).json(err))
+const createUser = async (req, res) => {
+    try {
+        const userData = await User.create(req.body)
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
 
-const updateUser = (req, res) => {
-    User.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body
-    )
-        .then(userData => !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData))
-        .catch(err => res.status(500).json(err))
+const updateUser = async (req, res) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body
+        )
+        !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
 
-const deleteUser = (req, res) => {
-    User.deleteOne(
-        { _id: req.params.id }
-    )
-        .then(userData => !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData))
-        .catch(err => res.status(500).json(err))
+const deleteUser = async (req, res) => {
+    try {
+        const userData = await User.deleteOne(
+            { _id: req.params.id }
+        )
+        !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
 
-const addFriend = (req, res) => {
-    User.findOneAndUpdate(
-        { id: req.params.userId },
-        { $addToSet: { friends: req.params.friendId } }
-    )
-        .then(userData => !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData))
-        .catch(err => res.status(500).json(err))
+const addFriend = async (req, res) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } }
+        )
+        !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData)
+    } catch (err) {
+        err => res.status(500).json(err)
+    }
 }
 
-const deleteFriend = (req, res) => {
-    User.findOneAndUpdate(
-        { id: req.params.userId },
-        { $pull: { friends: req.params.friendId } }
-    )
-        .then(userData => !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData))
-        .catch(err => res.status(500).json(err))
+const deleteFriend = async (req, res) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { id: req.params.userId },
+            { $pull: { friends: req.params.friendId } }
+        )
+        !userData ? res.status(404).json('User does not exist') : res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
 
 module.exports = { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, deleteFriend }
